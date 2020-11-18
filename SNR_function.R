ca67d42bce3ee13418874eb79313b60d021f2b2a
Audrey Bertin <audreybertin@Audreys-MacBook-Air.local>
2020-11-18 15:48:06 -0500

    added snr function + updated to-do

---
+++ SNR_function.R
@@ -0,0 +1,23 @@
+# Function to run the Simple Normal Regression template
+
+snr <- function(data,
+                x,
+                y,
+                n_chains = 4,
+                n_iter = 10000,
+                ci_pct = 95,
+                n_lines = 0,
+                posterior_prob = c(),
+                posterior_predict = c()){
+  
+  rmarkdown::render("SNR_Template.Rmd", params = list(
+    data = data,
+    x = x,
+    y = y,
+    n_chains = n_chains,
+    n_iter = n_iter,
+    n_lines = n_lines,
+    posterior_prob = posterior_prob,
+    posterior_predict = posterior_predict
+  ))
+}
